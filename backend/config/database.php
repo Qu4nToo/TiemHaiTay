02583f10f1818b60a6f8c096560ac0695a9a4e5f
Minstriz <dominhtri1788@gmail.com>
2024-11-21 18:35:17 +0700

    update backend

---
+++ backend/config/database.php
@@ -0,0 +1,15 @@
+<?php
+function getDatabaseConnection() {
+    $host = 'localhost';
+    $dbname = 'tiemhaitay';
+    $username = 'root';
+    $password = '';
+    
+    $conn = new mysqli($host, $username, $password, $dbname);
+    
+    if ($conn->connect_error) {
+        die("Kết nối thất bại: " . $conn->connect_error);
+    }
+    
+    return $conn;
+}
