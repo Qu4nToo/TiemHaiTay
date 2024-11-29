46c109b8bcbe3fc6bcbd8e1af2b80cef839faf72
Trần Nguyễn Hoàng Quân <tnhq1210@gmail.com>
2024-11-30 00:35:51 +0700

    push api products

--- backend/routes/api.php
+++ backend/routes/api.php
@@ -4,18 +4,12 @@ require_once '../controllers/userController.php';
 
 $action = $_GET['action'] ?? '';
 print_r($action);
-//exit;
-// Danh sách các action thuộc user và product
 $userActions = ['addUser', 'editUser', 'deleteUser'];
 $productActions = ['add', 'edit', 'delete'];
-
-// Phân loại action
 if (in_array($action, $userActions)) {
-    // Các hành động liên quan đến user
     handleRequestUser($action);
     exit;
 } elseif (in_array($action, $productActions)) {
-    // Các hành động liên quan đến product
     handleRequest($action);
     exit;
 } else {
